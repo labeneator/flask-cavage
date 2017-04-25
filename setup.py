0378d49236d4b3ba1e0d0be856ec43142bccc6ca
lmwangi <lmwangi@gmail.com>
2017-04-25 10:58:24 +0300

    bump revision to 0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Cavage',
-    version='0.1',
+    version='0.2',
     url='https://github.com/labeneator/flask_cavage',
     license='GPLv3',
     author='Laban Mwangi',
